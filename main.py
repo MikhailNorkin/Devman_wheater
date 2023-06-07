59085374da27e23e77d1a40b179c630a489de29b
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-06-07 14:01:21 +0300

    Comments deleted

--- main.py
+++ main.py
@@ -4,45 +4,14 @@ def print_town(town):
     payload = {'3':"", 'n':"", 'T':"", 'M':"", 'q':"", 'lang': 'ru'}
     adres = requests.get('https://wttr.in/' + town, params=payload)
     response = requests.get(adres.url)
-    # response = requests.get('https://wttr.in/' + town + '?3?n?T?M?q&lang=ru')
     response.raise_for_status()
     print(response.text)
 
 def main():
-    towns = ['san%20francisco','SVO','Череповец']
+    towns = ['London','SVO','Череповец']
 
     for town in towns:
         print_town(town)
-
-
-# Предыдущее задание: 
-
-    # url = 'https://wttr.in/san%20francisco?nTqu&lang=en'
-    # response = requests.get(url)
-    # response.raise_for_status()
-    # print(response.text)   
-
-    # url = 'https://wttr.in/London?3?n?T'
-    # response = requests.get(url)
-    # response.raise_for_status()
-    # print(response.text)
-
-    # url = 'https://wttr.in/Череповец?3?n?T'
-    # response = requests.get(url)
-    # response.raise_for_status()
-    # print(response.text)
-
-    # url = 'https://wttr.in/SVO?3?n?T'
-    # response = requests.get(url)
-    # response.raise_for_status()
-    # print(response.text)
-
-    # url = 'https://wttr.in/Череповец?3?n?T?M?q&lang=ru'
-    # response = requests.get(url)
-    # response.raise_for_status()
-    # print(response.text)
-
-# Конец предедыщего задания
 
 if __name__ == '__main__':
     main()
